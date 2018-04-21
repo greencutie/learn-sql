13e14050ef93bf9cba637762e9c67cf94b71a4ca
mutan <akim_now@mail.ru>
2018-04-21 22:32:34 +0300

    Fix formatting

--- db/migrations/20180408203450_create_emp_table.php
+++ db/migrations/20180408203450_create_emp_table.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Phinx\Migration\AbstractMigration;
 
 class CreateEmpTable extends AbstractMigration
--- db/seeds/DeptSeeder.php
+++ db/seeds/DeptSeeder.php
@@ -1,18 +1,9 @@
 <?php
 
-
 use Phinx\Seed\AbstractSeed;
 
 class DeptSeeder extends AbstractSeed
 {
-    /**
-     * Run Method.
-     *
-     * Write your database seeder using this method.
-     *
-     * More information on writing seeders is available here:
-     * http://docs.phinx.org/en/latest/seeding.html
-     */
     public function run()
     {
         $data = [
